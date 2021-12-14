006299412b94664d2753be17ee8698f70291683b
Odhran Daly <odhran.daly25@mail.dcu.ie>
2021-12-14 20:43:34 +0000

    day 10 part 1

--- src/main/java/dayTen/SyntaxScoring.java
+++ src/main/java/dayTen/SyntaxScoring.java
@@ -1,2 +1,49 @@
-package dayTen;public class SyntaxScoring {
+package dayTen;
+
+import java.util.*;
+
+public class SyntaxScoring {
+    public int getIllegalScore(String data) {
+        String[] syntaxLines = data.split("\\r?\\n");
+        List<Integer> corruptScores = new ArrayList<>();
+
+        for (String line : syntaxLines) {
+            corruptScores.add(getCorruptScore(line));
+        }
+
+        return corruptScores.stream().reduce(0, Integer::sum);
+    }
+
+    private Integer getCorruptScore(String line) {
+        Map<Character, Integer> syntaxErrorPoints = new HashMap<>(){
+            {
+                put(')', 3);
+                put(']', 57);
+                put('}', 1197);
+                put('>', 25137);
+            }
+        };
+        int score = 0;
+
+        Stack<Character> syntaxStack = new Stack<>();
+
+        for (int i = 0; i < line.length(); i++) {
+            char syntax = line.charAt(i);
+
+            if (syntax == '(' || syntax == '[' || syntax == '{' || syntax == '<') {
+                syntaxStack.push(syntax);
+            } else {
+                if ((syntaxStack.peek() == '(' && syntax == ')') ||
+                        (syntaxStack.peek() == '[' && syntax == ']') ||
+                        (syntaxStack.peek() == '{' && syntax == '}') ||
+                        (syntaxStack.peek() == '<' && syntax == '>')) {
+                    syntaxStack.pop();
+                } else {
+                    score = syntaxErrorPoints.get(syntax);
+                    break;
+                }
+            }
+        }
+        return score;
+    }
 }
--- src/test/java/dayTen/SyntaxScoringTest.java
+++ src/test/java/dayTen/SyntaxScoringTest.java
@@ -1,4 +1,40 @@
+package dayTen;
+
+import common.ReadFile;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SyntaxScoringTest {
-  
+
+    @Test
+    public void sampleData() {
+        SyntaxScoring scoring = new SyntaxScoring();
+        String data = "[({(<(())[]>[[{[]{<()<>>\n" +
+                "[(()[<>])]({[<{<<[]>>(\n" +
+                "{([(<{}[<>[]}>{[]{[(<()>\n" +
+                "(((({<>}<{<{<>}{[]{[]{}\n" +
+                "[[<[([]))<([[{}[[()]]]\n" +
+                "[{[{({}]{}}([{[{{{}}([]\n" +
+                "{<[[]]>}<{[{[{[]{()[[[]\n" +
+                "[<(<(<(<{}))><([]([]()\n" +
+                "<{([([[(<>()){}]>(<<{{\n" +
+                "<{([{{}}[<[[[<>{}]]]>[]]";
+        int illegalScore = scoring.getIllegalScore(data);
+        assertEquals(26397, illegalScore);
+    }
+
+    @Test
+    public void testData() throws IOException {
+        SyntaxScoring scoring = new SyntaxScoring();
+        ReadFile readFile = new ReadFile();
+        Path pathToFile = Path.of("/Users/odhrandaly/Dev/AdventOfCode/src/test/java/files/syntax.txt");
+        String data = readFile.getFileData(pathToFile);
+        int illegalScore = scoring.getIllegalScore(data);
+        assertEquals(26397, illegalScore);
+    }
+
 }
